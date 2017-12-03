478556a6857b83a602f46b97e54a6131c11a7396
Jan Dubniak <narusteras@gmail.com>
2017-12-03 14:28:42 +0100

    app.js +yargs command parsing
    TODO
    add db support

--- app.js
+++ app.js
@@ -4,10 +4,34 @@ const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
-let command = process.argv[2];
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
 
-console.log('Command: ', command);
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    } )
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+
+let command = process.argv[2];
 
 switch (command){
     case 'add':
